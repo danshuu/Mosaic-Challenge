0b2b11d6c6b0c65478546782b309aa6c114eb853
Daniel Shu <shudaniel95@gmail.com>
2019-04-26 13:19:21 -0400

    set up redux

---
+++ src/reducers/reducer.js
@@ -0,0 +1,10 @@
+import * as types from "./../constants/actionTypes";
+
+const initialState = {};
+
+const reducer = (state = initialState, { type, payload }) => {
+  const stateCopy = { ...state };
+  // const { type, payload } = action;
+};
+
+export default reducer;
